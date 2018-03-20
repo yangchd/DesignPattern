1a8e7f6d9dbe36efb9cf87583a1a5627071ecbe8
yangchd <yangchd@yonyou.com>
2018-03-20 18:35:48 +0800

    设计模式学习——适配器模式

--- src/main/java/com/yangchd/exercise/design/adapter/interadapter/WrapperTest.java
+++ src/main/java/com/yangchd/exercise/design/adapter/interadapter/WrapperTest.java
@@ -3,6 +3,7 @@ package com.yangchd.exercise.design.adapter.interadapter;
 /**
  * @author yangchd  2018/3/20.
  *
+ * 接口的适配器模式
  * 当不希望实现一个接口中所有的方法时，可以创建一个抽象类Wrapper，实现所有方法，我们写别的类的时候，继承抽象类即可
  */
 public class WrapperTest {
