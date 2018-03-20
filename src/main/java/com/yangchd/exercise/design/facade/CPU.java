2b920fb34d2f9bb4e464530245a8be73f3f2f4bb
yangchd <yangchd@yonyou.com>
2018-03-20 19:02:40 +0800

    设计模式学习——外观模式

---
+++ src/main/java/com/yangchd/exercise/design/facade/CPU.java
@@ -0,0 +1,15 @@
+package com.yangchd.exercise.design.facade;
+
+/**
+ * @author yangchd  2018/3/20.
+ */
+public class CPU {
+      
+    public void startup(){
+        System.out.println("cpu startup!");  
+    }  
+      
+    public void shutdown(){  
+        System.out.println("cpu shutdown!");  
+    }  
+}  
---
+++ src/main/java/com/yangchd/exercise/design/facade/Disk.java
@@ -0,0 +1,15 @@
+package com.yangchd.exercise.design.facade;
+
+/**
+ * @author yangchd  2018/3/20.
+ */
+public class Disk {
+      
+    public void startup(){  
+        System.out.println("disk startup!");  
+    }  
+      
+    public void shutdown(){  
+        System.out.println("disk shutdown!");  
+    }  
+}  
