1c701b9dcb7d63ac87b219326830d525c5f2b093
yangchd <yangchd@yonyou.com>
2018-03-20 18:57:15 +0800

    设计模式学习——装饰模式

--- src/main/java/com/yangchd/exercise/design/decorator/Test.java
+++ src/main/java/com/yangchd/exercise/design/decorator/Test.java
@@ -2,6 +2,11 @@ package com.yangchd.exercise.design.decorator;
 
 /**
  * @author yangchd  2018/3/20.
+ *
+ * 装饰模式
+ * 在不改变接口的前提下，动态扩展对象的访问。
+ * 动态继承，让类具有在运行期改变行为的能力。
+ * 装饰模式，突出的是运行期增加行为，这和继承是不同的，继承是在编译期增加行为。
  */
 public class Test {
 
