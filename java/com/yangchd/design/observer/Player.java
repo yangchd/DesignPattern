f1393cabbfc988a0d0fac4ea022362c5e2d4b8f3
yangchd <yangchd@yonyou.com>
2018-06-15 15:46:08 +0800

    设计模式

---
+++ java/com/yangchd/design/observer/Player.java
@@ -0,0 +1,37 @@
+package com.yangchd.design.observer;
+
+/**
+ * @author yangchd  2018/6/15.
+ */
+public class Player implements Observer {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void help() {
+        System.out.println("坚持住，" + this.name + "来救你！");
+    }
+
+    /**
+     * 遭受攻击方法的实现，当遭受攻击时将调用战队控制中心类的通知方法notifyObserver()来通知盟友
+     */
+    @Override
+    public void beAttacked(AbstractControlCenter acc) {
+        System.out.println(this.name + "被攻击！");
+        acc.notifyObserver(name);
+    }
+}
