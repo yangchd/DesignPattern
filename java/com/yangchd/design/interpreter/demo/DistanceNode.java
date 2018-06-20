f8860c0587eb8d69b62aa22071ca252aef830c8a
yangchd <yangchd@yonyou.com>
2018-06-20 15:52:30 +0800

    设计模式

---
+++ java/com/yangchd/design/interpreter/demo/DistanceNode.java
@@ -0,0 +1,19 @@
+package com.yangchd.design.interpreter.demo;
+
+/**
+ * @author yangchd  2018/6/20.
+ *
+ * 距离解释：终结符表达式
+ */
+public class DistanceNode extends AbstractNode {
+    private String distance;
+
+    public DistanceNode(String distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public String interpret() {
+        return this.distance;
+    }
+}
